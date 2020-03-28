eae3bd5563dd742bf5982e4c41327bf3af21e7d5
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2020-03-28 23:14:08 +0430

    BaseServiceProvider

---
+++ src/PressBaseServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Muhsenmaqsudi\Press;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class PressBaseServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        
+    }
+
+    public function register()
+    {
+        
+    }
+}
