368a706dfb29992cd0076465768e2fec85d9bf09
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2020-03-28 23:34:03 +0430

    In-Memory Sqlite Testing Database & adding posts migration

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -19,4 +19,13 @@ class TestCase extends TestCaseBase
             PressBaseServiceProvider::class,
         ];
     }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('database.default', 'testdb');
+        $app['config']->set('database.connections.testdb', [
+            'driver' => 'sqlite',
+            'database' => ':memory:'
+        ]);
+    }
 }
