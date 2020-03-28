61f92b99f6466dce2c51d91164ae093dbe33c7fa
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2020-03-28 18:50:13 +0430

    initial commit

---
+++ tests/Feature/InitialTest.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Muhsenmaqsudi\Press\Tests\Feature;
+
+
+use Orchestra\Testbench\TestCase;
+
+class InitialTest extends TestCase
+{
+    /** @test */
+    public function my_first_test()
+    {
+        $this->assertTrue(true);
+    }
+}
