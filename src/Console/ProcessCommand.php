33c1e0c2d6cff9777c72daaaf39819014cc8a356
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2020-03-29 11:11:27 +0430

    Config File & Laravel Publishable Package Assets

--- src/Console/ProcessCommand.php
+++ src/Console/ProcessCommand.php
@@ -18,7 +18,12 @@ class ProcessCommand extends Command
 
     public function handle()
     {
-        $files = File::files('blogs');
+        if (is_null(config('press'))) {
+            return $this->warn('Please publush the config file by running ' .
+                '\'php artisan vendor:publish --tag=press-config\'');
+        }
+
+        $files = File::files(config('press.path'));
 
         foreach ($files as $file) {
             $post = (new PressFileParser($file->getPathname()))->getData();
