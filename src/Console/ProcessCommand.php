5222eb1f259a54b339e72a19413116d31bead0cf
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2020-03-29 10:58:51 +0430

    building the Process Command

--- src/Console/ProcessCommand.php
+++ src/Console/ProcessCommand.php
@@ -5,6 +5,10 @@ namespace Muhsenmaqsudi\Press\Console;
 
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
+use Muhsenmaqsudi\Press\Post;
+use Muhsenmaqsudi\Press\PressFileParser;
 
 class ProcessCommand extends Command
 {
@@ -14,6 +18,18 @@ class ProcessCommand extends Command
 
     public function handle()
     {
-        $this->info('Hello');
+        $files = File::files('blogs');
+
+        foreach ($files as $file) {
+            $post = (new PressFileParser($file->getPathname()))->getData();
+        }
+
+        Post::create([
+            'identifier' => Str::random(),
+            'slug' => Str::slug($post['title']),
+            'title' => $post['title'],
+            'body' => $post['body'],
+            'extra' => $post['extra'] ?? [],
+        ]);
     }
 }
